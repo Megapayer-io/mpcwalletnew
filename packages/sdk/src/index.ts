0668d32750ae64b71b9b405d08fd5ff6e71e1e62
Hania <126733749+wajid404040@users.noreply.github.com>
2025-10-19 07:39:56 +0300

    Update index.ts

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,10 +3,13 @@ export type {
   Network,
   Keystore,
   WalletState,
+  Account,
   SendEthParams,
   SendErc20Params,
   TokenBalanceParams,
-  AddNetworkParams
+  AddNetworkParams,
+  ImportAccountParams,
+  CreateAccountParams
 } from './types.js';
 export { DEFAULT_NETWORKS } from './networks.js';
 export { encrypt, decrypt } from './crypto.js';
